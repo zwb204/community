9711dc8a69f902e0b24315f72e41797294521377
zwb <634700433@qq.com>
2019-10-31 14:47:46 +0800

    实现分页

--- src/main/java/life/zwb/community/Controller/IndexController.java
+++ src/main/java/life/zwb/community/Controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.zwb.community.Controller;
 
+import life.zwb.community.dto.PaginationDTO;
 import life.zwb.community.dto.QuestionDTO;
 import life.zwb.community.mapper.QuestionMapper;
 import life.zwb.community.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.jws.WebParam;
 import javax.servlet.http.Cookie;
@@ -32,7 +34,9 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest httpServletRequest, Model model){
+    public String index(HttpServletRequest httpServletRequest, Model model,
+                        @RequestParam(name = "page",defaultValue = "1")Integer page,
+                        @RequestParam(name = "size",defaultValue = "5")Integer size){
         Cookie[] cookies = httpServletRequest.getCookies();
 
         if (cookies != null && cookies.length != 0){
@@ -49,8 +53,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
