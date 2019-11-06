eeaeb847282e2891f36b798939ee26fa940c155d
zwb <634700433@qq.com>
2019-11-06 11:42:37 +0800

    解决不能加载资源问题

--- src/main/java/life/zwb/community/interceptor/WebConfig.java
+++ src/main/java/life/zwb/community/interceptor/WebConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  **/
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
