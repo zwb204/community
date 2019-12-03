30dfa88354afac0328fcd38baa1505f71a1166f8
zwb <634700433@qq.com>
2019-12-03 16:30:37 +0800

    实现回复功能——事务

--- src/main/java/life/zwb/community/service/CommentService.java
+++ src/main/java/life/zwb/community/service/CommentService.java
@@ -10,6 +10,7 @@ import life.zwb.community.model.Comment;
 import life.zwb.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Desc:
@@ -26,6 +27,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0){
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
