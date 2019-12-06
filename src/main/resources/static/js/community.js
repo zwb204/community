3e7a7a609a745e48431118627cdcb413ca7e6d45
zwb <634700433@qq.com>
2019-12-06 11:44:56 +0800

    实现问题回复——不刷新页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,7 +14,15 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code = 2003){
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=6624b7be4d40967c6e8f&redirect_uri=http://localhost:8880/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },
